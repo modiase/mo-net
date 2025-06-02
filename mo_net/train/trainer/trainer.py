330f9373d8c3267026d24cca93a179bd27bd5a59
Moye <moyeodiase@hotmail.co.uk>
2025-06-02 20:51:26 +0100

    Remove unused imports

--- mo_net/train/trainer/trainer.py
+++ mo_net/train/trainer/trainer.py
@@ -2,7 +2,6 @@ import time
 from collections.abc import Callable, Iterator, Sequence
 from contextlib import contextmanager, nullcontext
 from dataclasses import dataclass
-from functools import partial
 from pathlib import Path
 from typing import Any, ContextManager, Final, Literal, assert_never
 
