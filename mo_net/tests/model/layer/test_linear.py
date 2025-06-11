modiase/mnist-numpy

Place serialization logic on layer class
- Serialize logic within layer class.
- Use layer_id to identify which layer to apply updates to.
- Use new method of serialization to get updates from workers.
- Fix bug in linear backward prop computation of bias.
- Add test for parallel trainer serialization deserialization logic.

mnist_numpy/tests/model/layer/test_linear.py
@@ -1,8 +1,10 @@
+import io
 from dataclasses import dataclass
 
 import numpy as np
 import pytest
 
+from mnist_numpy.model.layer.base import BadLayerId
 from mnist_numpy.model.layer.linear import Linear, ParametersType
 from mnist_numpy.protos import Activations, Dimensions
 
@@ -544,3 +546,25 @@ def test_linear_gradient_operation_interface():
 
     layer.gradient_operation(grad_callback)
     assert called
+
+
+def test_linear_serialize_deserialize_parameters():
+    layer = Linear(input_dimensions=(2,), output_dimensions=(2,))
+    original_W = layer.parameters._W.copy()
+    original_B = layer.parameters._B.copy()
+    buffer = io.BytesIO()
+    layer.serialize_parameters(buffer)
+    buffer.seek(0)
+    layer.deserialize_parameters(buffer)
+    assert np.allclose(layer.parameters._W, original_W)
+    assert np.allclose(layer.parameters._B, original_B)
+
+
+def test_linear_serialize_deserialize_parameters_with_wrong_layer_id():
+    layer_1 = Linear(input_dimensions=(2,), output_dimensions=(2,))
+    layer_2 = Linear(input_dimensions=(2,), output_dimensions=(2,))
+    buffer = io.BytesIO()
+    layer_1.serialize_parameters(buffer)
+    buffer.seek(0)
+    with pytest.raises(BadLayerId):
+        layer_2.deserialize_parameters(buffer)

mnist_numpy/tests/model/test_model.py
@@ -5,6 +5,7 @@ import numpy as np
 import pytest
 from more_itertools import one
 
+from mnist_numpy.constants import N_BYTES_PER_FLOAT
 from mnist_numpy.model.block.base import Hidden
 from mnist_numpy.model.layer.linear import Linear
 from mnist_numpy.model.model import Model
@@ -107,3 +108,10 @@ def test_serialize_deserialize():
 
     assert model.block_dimensions == deserialized.block_dimensions
     assert np.allclose(X_prop_before, X_prop_after)
+
+
+def test_gradient_size():
+    model = Model.mlp_of(module_dimensions=((2,), (2,), (2,)))
+    assert model.parameter_count == 2 * 6  # 2 x (4 weights, 2 biases)
+    assert model.grad_layers[0].parameter_nbytes == 6 * N_BYTES_PER_FLOAT
+    assert model.grad_layers[1].parameter_nbytes == 6 * N_BYTES_PER_FLOAT

modiase/mo-net

mo_net/tests/model/layer/test_linear.py
@@ -1,8 +1,10 @@
+import io
 from dataclasses import dataclass
 
 import numpy as np
 import pytest
 
+from mo_net.model.layer.base import BadLayerId
 from mo_net.model.layer.linear import Linear, ParametersType
 from mo_net.protos import Activations, Dimensions
 
@@ -544,3 +546,25 @@ def test_linear_gradient_operation_interface():
 
     layer.gradient_operation(grad_callback)
     assert called
+
+
+def test_linear_serialize_deserialize_parameters():
+    layer = Linear(input_dimensions=(2,), output_dimensions=(2,))
+    original_W = layer.parameters._W.copy()
+    original_B = layer.parameters._B.copy()
+    buffer = io.BytesIO()
+    layer.serialize_parameters(buffer)
+    buffer.seek(0)
+    layer.deserialize_parameters(buffer)
+    assert np.allclose(layer.parameters._W, original_W)
+    assert np.allclose(layer.parameters._B, original_B)
+
+
+def test_linear_serialize_deserialize_parameters_with_wrong_layer_id():
+    layer_1 = Linear(input_dimensions=(2,), output_dimensions=(2,))
+    layer_2 = Linear(input_dimensions=(2,), output_dimensions=(2,))
+    buffer = io.BytesIO()
+    layer_1.serialize_parameters(buffer)
+    buffer.seek(0)
+    with pytest.raises(BadLayerId):
+        layer_2.deserialize_parameters(buffer)

mo_net/tests/model/test_model.py
@@ -5,6 +5,7 @@ import numpy as np
 import pytest
 from more_itertools import one
 
+from mo_net.constants import N_BYTES_PER_FLOAT
 from mo_net.model.block.base import Hidden
 from mo_net.model.layer.linear import Linear
 from mo_net.model.model import Model
@@ -107,3 +108,10 @@ def test_serialize_deserialize():
 
     assert model.block_dimensions == deserialized.block_dimensions
     assert np.allclose(X_prop_before, X_prop_after)
+
+
+def test_gradient_size():
+    model = Model.mlp_of(module_dimensions=((2,), (2,), (2,)))
+    assert model.parameter_count == 2 * 6  # 2 x (4 weights, 2 biases)
+    assert model.grad_layers[0].parameter_nbytes == 6 * N_BYTES_PER_FLOAT
+    assert model.grad_layers[1].parameter_nbytes == 6 * N_BYTES_PER_FLOAT